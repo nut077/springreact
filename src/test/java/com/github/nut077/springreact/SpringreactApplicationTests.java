9d94d4627f3ad05f2ebea4e71b8183888db6f9f0
nut077 <nuttapon.tavee@gmail.com>
2019-01-03 17:31:47 +0700

    Initial commit

---
+++ src/test/java/com/github/nut077/springreact/SpringreactApplicationTests.java
@@ -0,0 +1,17 @@
+package com.github.nut077.springreact;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringreactApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
