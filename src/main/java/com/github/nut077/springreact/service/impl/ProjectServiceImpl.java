31a80ca096f143c7f593c6dfa39a351a0c0b4835
nut077 <nuttapon.tavee@gmail.com>
2019-01-16 22:15:11 +0700

    add class Project

---
+++ src/main/java/com/github/nut077/springreact/service/impl/ProjectServiceImpl.java
@@ -0,0 +1,28 @@
+package com.github.nut077.springreact.service.impl;
+
+import com.github.nut077.springreact.entity.Project;
+import com.github.nut077.springreact.exception.ProjectIdException;
+import com.github.nut077.springreact.repository.ProjectRepository;
+import com.github.nut077.springreact.service.ProjectService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class ProjectServiceImpl implements ProjectService {
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project) {
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project Id '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+        }
+    }
+
+    @Override
+    public Project findByProjectIdentifier(String projectIdentifier) {
+        return null;
+    }
+}
